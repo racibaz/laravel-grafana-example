eea9940546ea833a9919c6b390e032c94ba7917e
raci <recai.cansiz@kidega.com>
2020-11-15 13:30:46 +0300

    first commit

---
+++ src/app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\UserRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = User::all();
+
+        return response()->json($users);
+    }
+
+    public function store(UserRequest $request)
+    {
+        $user =  User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+        return response()->json($user);
+    }
+}
